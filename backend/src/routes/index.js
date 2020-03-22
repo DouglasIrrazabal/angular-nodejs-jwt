cb678888aa3e647fe97528d69ab6811e1d20a39a
Douglas <douglasirrazabal@gmail.com>
2020-03-22 16:12:02 -0300

    32:08

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,15 +1,18 @@
 const { Router } = require('express');
 const router = Router();
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/users');
 
 router.get('/', (req, res) => res.send('Hello World'));
 
-router.post('/singup', (req, res) => {
+router.post('/singup', async (req, res) => {
     const { email, password } = req.body;
     const newUser = new User({email, password});
-    console.log(newUser);
-    res.send('well done');
+    await newUser.save();
+    
+    const token = jwt.sign({ _id: newUser._id }, 'secretKey');
+    res.status(200).json({token});
 });
 
 module.exports = router;
