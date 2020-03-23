29c0cc4b51b2bf90a6a84c9b80be09ecf80c67a6
Douglas <douglasirrazabal@gmail.com>
2020-03-22 21:00:54 -0300

    36:44

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -12,7 +12,19 @@ router.post('/singup', async (req, res) => {
     await newUser.save();
     
     const token = jwt.sign({ _id: newUser._id }, 'secretKey');
+    
     res.status(200).json({token});
+});
+
+router.post('/singin', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({email});
+    if (!user) return res.status(401).send("The email doesn't exists");
+    if (user.password !== password) return res.status(401).send("Wrong password");
+
+    const token = jwt.sign({_id: user._id}, 'secretKey');
+    return res.status(200).json({token});
+
 });
 
 module.exports = router;
